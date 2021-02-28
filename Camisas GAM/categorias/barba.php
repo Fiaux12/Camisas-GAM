81e1d511dac64fbf8caea33766f4ed42146968de
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-02-28 19:58:54 -0300

    troca: produto por mostraProduto

--- Camisas GAM/categorias/barba.php
+++ Camisas GAM/categorias/barba.php
@@ -112,7 +112,7 @@
 												?>
 												<footer>
 													<ul class="actions">
-													<form action="../produto.php" method="POST">
+													<form action="../mostraProduto.php" method="POST">
 															<input type="hidden" name="identificador" value="31" />
 															<button>Comprar</button>
 													    </form>
