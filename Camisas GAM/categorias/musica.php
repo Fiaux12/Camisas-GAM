f8103e64fb37507b409251a2ba4a23acbf11357e
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-02-28 20:07:05 -0300

    produto por mostraProduto

--- Camisas GAM/categorias/musica.php
+++ Camisas GAM/categorias/musica.php
@@ -112,7 +112,7 @@
 												?>
 												<footer>
 													<ul class="actions">
-													<form action="../produto.php" method="POST">
+													<form action="../mostraProduto.php" method="POST">
 															<input type="hidden" name="identificador" value="24" />
 															<button>Comprar</button>
 													    </form>
@@ -144,7 +144,7 @@
 												?>
 												<footer>
 													<ul class="actions">
-													<form action="../produto.php" method="POST">
+													<form action="../mostraProduto.php" method="POST">
 															<input type="hidden" name="identificador" value="33" />
 															<button>Comprar</button>
 													    </form>
