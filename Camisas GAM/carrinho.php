7589cba47488c70944938bdc1fa1cf4ee8642204
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-02-28 19:47:44 -0300

    mudança em: carrinho.php e mostraPorduto.php

--- Camisas GAM/carrinho.php
+++ Camisas GAM/carrinho.php
@@ -4,14 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>carrinho</title>
 </head>
 <body>
+    <h1>Carrinho</h1>
     <?php
         require 'conexao.php';
+        require 'Carrinho.class.php';
+
+        $c = new Carrinho();
+       
+        $id = $_POST['idCamisa'];
+        $quantidade = $_POST['quantidade'];
+        $cor = $_POST['cor'];
+        $sexo = $_POST['sexo'];
+        $tamanho = $_POST['tamanho'];
+        $CEP =  $_POST['CEP'];
+        
+        $c->adicionar($id, $quantidade, $sexo, $cor, $CEP, $tamanho);
 
         foreach($_SESSION['carrinho'] as $produto){
-            echo $produto[0];
+            echo "session: " . $produto[0];
+            echo ("<br>");
+            foreach($produto as $coisa){
+                echo $coisa;
+                echo ("<br>");
+
+            }
         }
 
     
--- Camisas GAM/mostraProduto.php
+++ Camisas GAM/mostraProduto.php
@@ -15,7 +15,23 @@
 
 	</head>
 <body>
- <img src="placeholder.jpg" alt="placeholder" align="left" width="360" height="640">
+
+  <?php
+
+    $dados = $p->buscaDadosLinha($_POST['identificador']);
+    if(count($dados) > 0){
+      for($i=0; $i < count($dados); $i++){
+        foreach ($dados[$i] as $k => $v) {
+          if($k == "img"){
+            //echo $v;
+            echo '<img src="'.$v.'" alt="placeholder" align="left" width="360" height="580">';
+          }
+        }
+      }
+    }
+  ?>
+
+ 
  <div class="centro">
         <h1 id="cad-log">
           <?php
@@ -31,69 +47,87 @@
             }
           ?>
         </h1>
-        <h3 id="cad-log">genero: Daora cor: Daora</h3>
       </div>
- <h1 id="cad-log">Coloque as opções para seu pedido</h1>
- <form action="" method="POST" class="centro">
+ <h2 style="color: white;" id="cad-log">Coloque as opções para seu pedido</h2>
+  <form action="carrinho.php" method="POST" class="centro">
 		<fieldset>
           <p>
              <label id="cad-log">Tamanho</label>
-             <select id = "myList">
-               <option value = "1">PP</option>
-               <option value = "2">P</option>
-               <option value = "3">M</option>
-               <option value = "4">G</option>
-               <option value = "5">2G</option>
+             <select id = "myList" name="tamanho">
+               <option value = "PP">PP</option>
+               <option value = "P">PP</option>
+               <option value = "M">M</option>
+               <option value = "G">G</option>
+               <option value = "GG">GG</option>
+               <option value = "XG">XG</option>
+
              </select>
           </p>
-       </fieldset>
-       		<fieldset>
-          <p>
-             <label id="cad-log">Sexo</label>
-             <select id = "myList">
-               <option value = "1">Masculino</option>
-               <option value = "2">Feminino</option>
-               <option value = "3">Unissex</option>
-             </select>
-          </p>
-       </fieldset>
-       <fieldset>
-          <p>
-             <label id="cad-log">Cor</label>
-             <select id = "myList">
-               <option value = "1">Preto</option>
-               <option value = "2">Branco</option>
-               <option value = "3">Rosa</option>
-               <option value = "4">Azul</option>
-               <option value = "5">Amarelo</option>
-               <option value = "6">Vermelho</option>
-               <option value = "7">Verde</option>
-               <option value = "8">Roxo</option>
-               <option value = "9">Laranja</option>
-             </select>
-          </p>
-       </fieldset>
-       <fieldset>
-       	<div class="two-col">
-    <div class="col1">
-       <label id="cad-log">Quantidade</label> <input type="number" name="quantidade">
-   </div>
-   <div class="col2">
-       <label id="cad-log">CEP(apenas números)</label> <input type="number" name="CEP">
-       <button type="submit"><i class="fa fa-shopping-cart"></i>  Comprar<br>R$69,00</button>
-   </div>
-</div>
+    </fieldset>
+    <fieldset>
+      <p>
+          <label id="cad-log">Sexo</label>
+          <select id = "myList" name="sexo">
+            <option value = "Masculino">Masculino</option>
+            <option value = "Feminino">Feminino</option>
+            <option value = "Unissex">Unissex</option>
+          </select>
+      </p>
+    </fieldset>
+    <fieldset>
+      <p>
+          <label id="cad-log">Cor</label>
+          <select id = "myList" name="cor">
+            <option value = "Preto">Preto</option>
+            <option value = "Branco">Branco</option>
+            <option value = "Rosa">Rosa</option>
+            <option value = "Azul">Azul</option>
+            <option value = "Amarelo">Amarelo</option>
+            <option value = "Vermelho">Vermelho</option>
+            <option value = "Verde">Verde</option>
+            <option value = "Roxo">Roxo</option>
+            <option value = "Laranja">Laranja</option>
+          </select>
+      </p>
+    </fieldset>
+    <fieldset>
+    
+    <div class="two-col">
+      <div class="col1">
+        <label id="cad-log">Quantidade</label> <input type="number" name="quantidade">
+      </div>
+      <div class="col2">
+        <label id="cad-log">CEP(apenas números)</label> <input type="number" name="CEP">
+          <?php
+            $dados = $p->buscaDadosLinha($_POST['identificador']);
+            if(count($dados) > 0){
+              for($i=0; $i < count($dados); $i++){
+                foreach ($dados[$i] as $k => $v) {
+                  if($k == "id"){
+                    $idCamisa = $v;
+                  }
+                  if($k == "preco"){
+
+                    echo '<button type = "submit" name = "idCamisa" value="'.$idCamisa.'"><i class="fa fa-shopping-cart"></i>  Comprar<br>R$'.$v.'</button>';
+										
+                  }
+                }
+              }
+            }
+          ?>
+      </div>
+    </div>
        	</fieldset>
-       </form>
-       <div class="centro" style="width: 30%; display: table;">
-       <div class="centro" style="display: table-row; height: 1px;">
-            <div style="width: 2%; display: table-cell;">
-      <i class="fa fa-truck"></i>
+  </form>
+  <div class="centro" style="width: 30%; display: table;">
+    <div class="centro" style="display: table-row; height: 1px;">
+      <div style="width: 2%; display: table-cell;">
+        <i class="fa fa-truck"></i>
+      </div>
+      <div class="centro" style="display: table-cell;">
+        <p id="cad-log">O frete é grátis, não se preocupe</p>
+      </div>
+    </div>
   </div>
-  <div class="centro" style="display: table-cell;">
-      <p id="cad-log">O frete é grátis, não se preocupe</p>
-  </div>
-</div>
-</div>
       </body>
 </html>
