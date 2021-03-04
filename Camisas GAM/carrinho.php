10ad3bd19457a8a77e834e40af53486211987ae5
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-03-04 20:32:53 -0300

    carrinho vazio

--- Camisas GAM/carrinho.php
+++ Camisas GAM/carrinho.php
@@ -24,51 +24,62 @@
         <h1 style="color: white; font-size:30px; text-align: center; " >Bem Vindo ao Carrinho de Compras!</h1>
         <br><br>
         <?php
-            //session_destroy();
-            $total = 0.00;
-            foreach($_SESSION['carrinho'] as $dadosPedido){
-                
-                echo '<fieldset class="centro-borda">';
-                    echo '<div class="centro">';
-                        echo '<img src="'. $dadosPedido[4] .'"  align="left" width="300" height="320">';
-                        echo '<b><p id="car">'.$dadosPedido[3].'</p></b>';
-                        echo '<p id="car">Tamanho: '.$dadosPedido[8].'</p>';
-                        echo '<p id="car">Cor: '.$dadosPedido[7].'</p>';
-                        echo '<p id="car">Sexo: '.$dadosPedido[6].'</p>';
-                        echo '<p  style="color: white;" id="cad">Quantidade:'.$dadosPedido[5].'</p> ';
-                        echo '<p id="car">R$'.$dadosPedido[2].'</p>';
-                    echo '</div>';
-                echo '</fieldset>';
-
-                $total += $dadosPedido[2] * $dadosPedido[5];
-            }
+            if(!isset($_SESSION['carrinho'])){
         ?>
-        <div class="cent" style=" text-align: center;">
-            <form action="pagar.php" method="POST">
-                <?php
+                <div style="text-align: center;">
+                    <h3 id="cad-log">Selecione algum produto para coloca-lo no carrinho!</h3>
+                    <p id="cad-log">Volte para nosso catálogo.</p>
+                    <a href="./index.php"><button type="button" class="btn btn-primary">Home</button></a>
+                </div>
+        <?php
+            }else{
+            
+                $total = 0.00;
+                foreach($_SESSION['carrinho'] as $dadosPedido){
                     
-                echo'<button  type = "submit" name = "btncomprar" value = "'.$total.'" class="centro"><i class="fa fa-shopping-cart"></i>Finalizar Compra<br>R$'.$total.'</button>';
-                    
-                ?>
-            </form>
-            <br>
-            <form action="index.php" method="POST">
-                <?php
-                    
-                echo'<button  type = "submit" class="centro">Voltar às compras</button>';
-                    
-                ?>
-            </form>
-        </div>
-         <div class="centro" style="width: 30%; display: table;">
-       <div class="centro" style="display: table-row; height: 1px;">
-            <div style="width: 2%; display: table-cell;">
-      <i class="fa fa-truck" style=" color: rgb(177,210,119);"></i>
-  </div>
-  <div class="centro" style="display: table-cell;">
-      <p  style=" color: rgb(177,210,119);" id="cad-log">O frete é grátis! Não se preocupe.</p>
-  </div>
-</div>
-</div>
+                    echo '<fieldset class="centro-borda">';
+                        echo '<div class="centro">';
+                            echo '<img src="'. $dadosPedido[4] .'"  align="left" width="300" height="320">';
+                            echo '<b><p id="car">'.$dadosPedido[3].'</p></b>';
+                            echo '<p id="car">Tamanho: '.$dadosPedido[8].'</p>';
+                            echo '<p id="car">Cor: '.$dadosPedido[7].'</p>';
+                            echo '<p id="car">Sexo: '.$dadosPedido[6].'</p>';
+                            echo '<p  style="color: white;" id="cad">Quantidade:'.$dadosPedido[5].'</p> ';
+                            echo '<p id="car">R$'.$dadosPedido[2].'</p>';
+                        echo '</div>';
+                    echo '</fieldset>';
+                    $total += $dadosPedido[2] * $dadosPedido[5];
+                }
+        ?>
+            <div class="cent" style=" text-align: center;">
+                <form action="pagar.php" method="POST">
+                    <?php
+                        
+                    echo'<button  type = "submit" name = "btncomprar" value = "'.$total.'" class="centro"><i class="fa fa-shopping-cart"></i>Finalizar Compra<br>R$'.$total.'</button>';
+                        
+                    ?>
+                </form>
+                <br>
+                <form action="index.php" method="POST">
+                    <?php
+                        
+                    echo'<button  type = "submit" class="centro">Voltar às compras</button>';
+                        
+                    ?>
+                </form>
+            </div>
+            <div class="centro" style="width: 30%; display: table;">
+                <div class="centro" style="display: table-row; height: 1px;">
+                    <div style="width: 2%; display: table-cell;">
+                        <i class="fa fa-truck" style=" color: rgb(177,210,119);"></i>
+                    </div>
+                    <div class="centro" style="display: table-cell;">
+                        <p  style=" color: rgb(177,210,119);" id="cad-log">O frete é grátis! Não se preocupe.</p>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
 	</body>
 	</html>
