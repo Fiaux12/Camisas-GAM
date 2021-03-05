b3090427f52392d6eee4cb34fc7a56d81e56d5ef
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-03-05 15:32:02 -0300

    Add files via upload

--- Camisas GAM/sair.php
+++ Camisas GAM/sair.php
@@ -2,5 +2,7 @@
     session_start();
     unset($_SESSION['idUsuario']);
     unset($_SESSION['carrinho']);
+    unset($_SESSION['emailUsuario']);
+    unset($_SESSION['nomeUsuario']);
     header("location: index.php");
 ?>
