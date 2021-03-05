f4376fe28245069fb4bd6c3563e0bd09b4d211f6
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-03-05 15:52:35 -0300

    Add files via upload

---
+++ Camisas GAM/salvarCadastro.php
@@ -0,0 +1,27 @@
+<?php
+
+    if (isset($_POST['nome']) && !empty($_POST['nome']) && isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) && !empty($_POST['senha'])) {
+
+        require 'conexao.php';
+        require 'Usuario.class.php';
+
+        $u = new Usuario();
+        
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $tipoUsuario = 'cliente';
+        $senha = $_POST['senha'];
+
+        if($u->cadastro($nome, $email, $tipoUsuario, $senha) == true){
+                header("Location: index.php"); 
+        }else{
+            $valida = 'invalido';
+            $_SESSION['cadastro'] = $valida;
+            header("Location: cadastro.php");
+        }
+
+    }else{
+        header("Location: cadastro.php");
+    }
+
+?>
