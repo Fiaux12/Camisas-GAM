d96dfc3353aa61c4b1be6c49dbd36d5e71b6248d
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-03-09 17:50:06 -0300

    Add files via upload

--- Camisas GAM/criarPreferencias.php
+++ Camisas GAM/criarPreferencias.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-		<title>Produto</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-	</head>
-    <body>
 <?php
 
     if (isset($_POST['addPreferencia'])) {
@@ -37,6 +27,3 @@
         
     }
 ?>
-
-    </body>
-</html>
