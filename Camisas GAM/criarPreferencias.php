323e9f4f04f84e7c1012e2f16e962c2aab77719a
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2021-03-10 13:28:02 -0300

    Add files via upload

--- Camisas GAM/criarPreferencias.php
+++ Camisas GAM/criarPreferencias.php
@@ -6,24 +6,30 @@
         require 'conexao.php';
         require 'Pedido.class.php';
 
-        $p = new Pedido();
-        
-        $idCamisa = $_POST['addPreferencia'];
-        $idConta = $_SESSION['idUsuario'];
-
-        global $pdo;
-        $sql = "SELECT id_camisa FROM Preferencia WHERE id_camisa = '$idCamisa' AND id_conta = '$idConta';";
-        $sql = $pdo->prepare($sql);
-        $sql->execute();
-       
-        if($sql->rowCount() > 0){
-            $dado = $sql->fetch();
-            echo $dado['id_camisa'];
+        if (!isset($_SESSION['idUsuario'])) {
             header("Location: index.php");
         }else {
-            $p->criaPreferencia($idCamisa, $idConta);
-            header("Location: index.php");
+            $p = new Pedido();
+        
+            $idCamisa = $_POST['addPreferencia'];
+            $idConta = $_SESSION['idUsuario'];
+            
+            global $pdo;
+            $sql = "SELECT id_camisa FROM Preferencia WHERE id_camisa = '$idCamisa' AND id_conta = '$idConta';";
+            $sql = $pdo->prepare($sql);
+            $sql->execute();
+        
+            if($sql->rowCount() > 0){
+                $dado = $sql->fetch();
+                echo $dado['id_camisa'];
+                header("Location: index.php");
+            }else {
+                $p->criaPreferencia($idCamisa, $idConta);
+                header("Location: index.php");
+            }
         }
+
+        
         
     }
 ?>
